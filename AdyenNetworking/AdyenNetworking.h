1654e8332fb7be392e0fa442402e8be9cce36648
mohamede <mohamed.eldoheiri@adyen.com>
2021-07-27 11:41:50 +0200

    Adds the generic Http networking classes and structs

---
+++ AdyenNetworking/AdyenNetworking.h
@@ -0,0 +1,18 @@
+//
+//  AdyenNetworking.h
+//  AdyenNetworking
+//
+//  Created by Mohamed Eldoheiri on 7/27/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdyenNetworking.
+FOUNDATION_EXPORT double AdyenNetworkingVersionNumber;
+
+//! Project version string for AdyenNetworking.
+FOUNDATION_EXPORT const unsigned char AdyenNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdyenNetworking/PublicHeader.h>
+
+
---
+++ AdyenNetworking/Helpers/Logger.swift
@@ -0,0 +1,27 @@
+//
+// Copyright (c) 2021 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import func Darwin.fputs
+
+/// Provides control over SDK logging.
+public enum AdyenLogging {
+    /// Indicates whether to enable printing to the console.
+    public static var isEnabled: Bool = false
+}
+
+/// :nodoc:
+/// Copies the interface of `Swift.print()`,
+/// and `Swift.print()` is called inside after checking first if `AdyenLogging.isEnabled` is `true`, and returns if `false`.
+public func adyenPrint(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    guard AdyenLogging.isEnabled else { return }
+    var idx = items.startIndex
+    let endIdx = items.endIndex
+    
+    repeat {
+        Swift.print(items[idx], separator: separator, terminator: idx == (endIdx - 1) ? terminator : separator)
+        idx += 1
+    } while idx < endIdx
+}
