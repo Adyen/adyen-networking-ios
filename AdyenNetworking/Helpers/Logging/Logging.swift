9a805e34a6f2df244ef224096fcca4bf57c00af6
Alex Guretzki <alexander.guretzki@adyen.com>
2025-03-13 13:16:08 +0100

    removing unneeded global func

--- AdyenNetworking/Helpers/Logging/Logging.swift
+++ AdyenNetworking/Helpers/Logging/Logging.swift
@@ -15,11 +15,6 @@ public enum Logging {
 // MARK: - Backwards compatible global print
 
 @_spi(AdyenInternal)
-public func adyenPrintAsJSON(_ dictionary: [String: Any], fileId: String = #fileID) {
-    DebugLogger().printAsJSON(dictionary, fileId: fileId)
-}
-
-@_spi(AdyenInternal)
 public func adyenPrintAsJSON(_ data: Data, fileId: String = #fileID) {
     DebugLogger().printAsJSON(data, fileId: fileId)
 }
